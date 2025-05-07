0706049e3bd64a6fd94babcfaaed2052d39518c3
elizaveetaPopova <eli.bychkova@gmail.com>
2025-05-07 10:46:42 +0300

    added models

---
+++ db/models/review.model.ts
@@ -0,0 +1,39 @@
+import {
+  Table,
+  Column,
+  DataType,
+  Model,
+  ForeignKey,
+  BelongsTo,
+} from 'sequelize-typescript';
+import User from './user.model';
+import Movie from './movie.model';
+
+@Table({
+  tableName: 'reviews',
+  timestamps: true,
+  underscored: true,
+})
+export default class Review extends Model {
+  @Column(DataType.STRING)
+  text: string;
+
+  @Column({
+    type: DataType.FLOAT,
+    allowNull: false,
+    defaultValue: 1,
+  })
+  rating: number;
+
+  @ForeignKey(() => Movie)
+  movieId: number;
+
+  @ForeignKey(() => User)
+  userId: number;
+
+  @BelongsTo(() => Movie)
+  movie: Movie;
+
+  @BelongsTo(() => User)
+  user: User;
+}
---
+++ db/models/user-movie-status.model.ts
@@ -0,0 +1,38 @@
+import {
+  BelongsTo,
+  Column,
+  DataType,
+  ForeignKey,
+  Model,
+  Table,
+} from 'sequelize-typescript';
+import User from './user.model';
+import Movie from './movie.model';
+
+@Table({
+  tableName: 'user_movie_statuses',
+  timestamps: true,
+  underscored: true,
+})
+export default class UserMovieStatus extends Model {
+  @ForeignKey(() => User)
+  @Column
+  userId: number;
+
+  @ForeignKey(() => Movie)
+  @Column
+  movieId: number;
+
+  @Column({
+    allowNull: false,
+    defaultValue: 'watching',
+    type: DataType.ENUM('want_to_watch', 'watched'),
+  })
+  status: 'want_to_watch' | 'watched';
+
+  @BelongsTo(() => User)
+  user: User;
+
+  @BelongsTo(() => Movie)
+  movie: Movie;
+}
