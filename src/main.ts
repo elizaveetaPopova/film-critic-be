623e95a238abe4a3f59a482ef25ebc2a991a2565
elizaveetaPopova <eli.bychkova@gmail.com>
2025-05-13 13:59:36 +0300

    added useGlobalPipes and ConfigService to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
+  await app.listen(configService.get('PORT') ?? 3000);
 }
 bootstrap();
